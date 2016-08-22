80e2d156dae793fd77c14ae63f3fd6c4e77e3704
dinosaadeh <dinosaadeh@gmail.com>
2016-08-22 18:42:37 +0300

    - work in progress main menu

--- core/src/com/smb215team/barjis/screens/MenuScreen.java
+++ core/src/com/smb215team/barjis/screens/MenuScreen.java
@@ -9,12 +9,40 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.smb215team.barjis.game.Assets;
+import com.smb215team.barjis.util.Constants;
+
 /**
  *
  * @author dinosaadeh
  */
 public class MenuScreen extends AbstractGameScreen {
     private static final String TAG = MenuScreen.class.getName();
+    private Stage stage;
+    private Image mainScreenLogo;
+    
+    // debug
+    private final float DEBUG_REBUILD_INTERVAL = 5.0f;
+    private boolean debugEnabled = false;
+    private float debugRebuildStage;
     
     public MenuScreen (Game game) {
         super(game);
@@ -28,18 +56,58 @@ public class MenuScreen extends AbstractGameScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         if(Gdx.input.isTouched())
             game.setScreen(new GameScreen(game));
+        
+        if (debugEnabled) {
+            debugRebuildStage -= deltaTime;
+            if (debugRebuildStage <= 0) {
+                debugRebuildStage = DEBUG_REBUILD_INTERVAL;
+                rebuildStage();
+            }
+        }
+        stage.act(deltaTime);
+        stage.draw();
     }
     
-    @Override public void resize (int width, int height) { }
+    @Override public void resize (int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
     
-    @Override public void show () { }
+    @Override public void show () {
+        stage = new Stage(new FillViewport(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT));
+        Gdx.input.setInputProcessor(stage);
+        rebuildStage();
+    }
     
-    @Override public void hide () { }
+    @Override public void hide () {
+        stage.dispose();
+    }
     
     @Override public void pause () { }
 
     @Override
     public InputProcessor getInputProcessor() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private void rebuildStage () {
+        // assemble stage for menu screen
+        stage.clear();
+        Stack stack = new Stack();
+        stage.addActor(stack);
+        stack.setSize(Constants.VIEWPORT_GUI_WIDTH, Constants.VIEWPORT_GUI_HEIGHT);
+        
+        // build all layers
+        Table layerBackground = buildBackgroundLayer();
+        
+        // assemble stage for menu screen
+        stack.add(layerBackground);
+    }
+    
+    private Table buildBackgroundLayer () {
+        Table layer = new Table();
+        // + Background
+        //mainScreenLogo = new Image(skinCanyonBunny, "background");
+        layer.add(mainScreenLogo);
+        return layer;
     }
 }
