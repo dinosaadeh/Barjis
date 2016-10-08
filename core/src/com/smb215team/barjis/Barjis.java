208521f4f301bab58f6909c5b3df3dcdb3df6976
dinosaadeh <dinosaadeh@gmail.com>
2016-10-08 16:23:05 +0300

    - removed unneeded test method calls trying to connect to nodejs server.

--- core/src/com/smb215team/barjis/Barjis.java
+++ core/src/com/smb215team/barjis/Barjis.java
@@ -5,13 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.smb215team.barjis.game.Assets;
 import com.smb215team.barjis.screens.MenuScreen;
-import com.smb215team.barjis.game.objects.*;
-import io.socket.client.IO;
 import io.socket.client.Socket;
 
 public class Barjis extends Game {
     private static final String TAG = Barjis.class.getName();
-    private Socket socket;///socket communicating between Server and Client
 
     @Override
     public void create () {
@@ -20,17 +17,6 @@ public class Barjis extends Game {
         // Load assets
         Assets.instance.init(new AssetManager());
         // Start game at menu screen
-        setScreen(new MenuScreen(this)); 
-        connectSocket();///function responsible of socket configuration
-    }
-    public void connectSocket(){
-        try{
-            socket =IO.socket("http://0.0.0.0:8082");
-            socket.connect(); 
-        }catch (Exception e){
-       //     System.out.println(e);
-       Gdx.app.log(TAG, e.toString());
-        }
-            
+        setScreen(new MenuScreen(this));
     }
 }
