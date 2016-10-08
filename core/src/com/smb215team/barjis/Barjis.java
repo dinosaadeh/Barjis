d84a97473c67880148c6c6a92eb95d67bf3192ce
dinosaadeh <dinosaadeh@gmail.com>
2016-10-08 16:44:42 +0300

    - merge

--- core/src/com/smb215team/barjis/Barjis.java
+++ core/src/com/smb215team/barjis/Barjis.java
@@ -5,10 +5,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.smb215team.barjis.game.Assets;
 import com.smb215team.barjis.screens.MenuScreen;
+
+import io.socket.client.IO;
 import io.socket.client.Socket;
 
 public class Barjis extends Game {
     private static final String TAG = Barjis.class.getName();
+    private Socket socket;///socket communicating between Server and Client
+
+    public final com.smb215team.barjis.facebook.FacebookService facebookService;
+
+    public Barjis(com.smb215team.barjis.facebook.FacebookService facebookService) {
+        this.facebookService = facebookService;
+    }
 
     @Override
     public void create () {
@@ -17,7 +26,18 @@ public class Barjis extends Game {
         // Load assets
         Assets.instance.init(new AssetManager());
         // Start game at menu screen
-        setScreen(new MenuScreen(this));
+        setScreen(new MenuScreen(this)); 
+        connectSocket();///function responsible of socket configuration
+    }
+    public void connectSocket(){
+        try{
+            socket =IO.socket("http://0.0.0.0:8082");
+            socket.connect(); 
+        }catch (Exception e){
+       //     System.out.println(e);
+       Gdx.app.log(TAG, e.toString());
+        }
+            
     }
 
 }
